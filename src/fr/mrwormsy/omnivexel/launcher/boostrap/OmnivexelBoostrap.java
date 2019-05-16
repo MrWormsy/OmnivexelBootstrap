3013e75bf644c641ef35ef3480a15dda873cf6fa
MrWormsy <antonin.martin.rosa@gmail.com>
2019-05-16 21:15:41 +0200

    Now detect when connected to the internet

--- src/fr/mrwormsy/omnivexel/launcher/boostrap/OmnivexelBoostrap.java
+++ src/fr/mrwormsy/omnivexel/launcher/boostrap/OmnivexelBoostrap.java
@@ -2,7 +2,11 @@ package fr.mrwormsy.omnivexel.launcher.boostrap;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+
+import javax.swing.JOptionPane;
 
 import fr.theshark34.supdate.SUpdate;
 import fr.trxyy.launcherlib.Init;
@@ -16,9 +20,24 @@ public class OmnivexelBoostrap {
 	
 	private static long now;
 	
+	static BoostrapFrame bf;
+	
 	public static void main(String[] args) {
 		
-		BoostrapFrame bf = new BoostrapFrame();
+		Init.registerLauncherConfiguration("", 0, 0, "omnivexelproject/launcher", "/resources/");
+		
+		//Check if the guy is connected to the internet (to download the launcher)
+		bf = new BoostrapFrame();
+		
+		if (!netIsAvailable()) {
+			// If we are here that means we are not connected to the internet... We have to cases, either the guy has already download the game and if not we tell him that he needs internet
+			if (new File(OSUtil.getPath() + "\\OmnivexelLauncher.jar").exists()) {
+				launchLauncher();
+			} else {
+				JOptionPane.showMessageDialog(bf, "ERROR : You are not connected to the internet, and you don't have the launcher installed.", "No internet connection", JOptionPane.ERROR_MESSAGE);				
+				System.exit(0);
+			}
+		}
 		
 		now = System.currentTimeMillis();
 		
@@ -36,7 +55,7 @@ public class OmnivexelBoostrap {
 			}
 		}.start();
 		
-		Init.registerLauncherConfiguration("", 0, 0, "omnivexelproject/launcher", "/resources/");
+		
 		
 		su = new SUpdate("http://51.75.254.98/omnivexel/", OSUtil.getDirectory());
 		
@@ -50,6 +69,7 @@ public class OmnivexelBoostrap {
 	}
 	
 	public static void update() throws Exception {
+		
 		su.getFileManager().download(new URL("http://51.75.254.98/omnivexel/OmnivexelLauncher.jar"), new File(OSUtil.getPath() + "\\OmnivexelLauncher.jar"));
 		
 		while (! new File(OSUtil.getPath() + "\\OmnivexelLauncher.jar").exists()) {}
@@ -67,6 +87,19 @@ public class OmnivexelBoostrap {
 		}
 		
         System.exit(0);
-	}
+	}	
 	
+	private static boolean netIsAvailable() {
+	    try {
+	        final URL url = new URL("http://www.google.com");
+	        final URLConnection conn = url.openConnection();
+	        conn.connect();
+	        conn.getInputStream().close();
+	        return true;
+	    } catch (MalformedURLException e) {
+	        throw new RuntimeException(e);
+	    } catch (IOException e) {
+	        return false;
+	    }
+	}
 }
